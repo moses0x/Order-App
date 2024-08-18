82a860c29b352dd797803d988f6e6f4f3f44518a
Moses <mosesajila@gmail.com>
2024-08-18 23:53:56 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Cart from "./component/Cart.jsx";
+import Checkout from "./component/Checkout.jsx";
+import Header from "./component/Header.jsx";
+import Meals from "./component/Meals.jsx";
+import { CartContextProvider } from "./store/CartContext.jsx";
+import { UserProgressContextProvider } from "./store/UserProgressContext.jsx";
+
+function App() {
+  return (
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart />
+        <Checkout />
+      </CartContextProvider>
+    </UserProgressContextProvider>
+  );
+}
+
+export default App;
